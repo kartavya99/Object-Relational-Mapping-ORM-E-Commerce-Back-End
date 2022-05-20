5fed518b1d8da39921ca1d79966b7b69db16d01a
kartavya <kartavyas@gmail.com>
2022-05-20 10:47:29 +1000

    updated env file and server started listening

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require("./config/connection");
 
+//setting up express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//importing middleware 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// turn on routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
