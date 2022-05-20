16bf0f924ccaa6eca33522224e297a124527c9e6
kartavya <kartavyas@gmail.com>
2022-05-20 11:26:04 +1000

    updated product model

--- models/Product.js
+++ models/Product.js
@@ -9,12 +9,41 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
-    // id - Int, not Null, PK and auto Inc
+     // id - Int, not Null, PK and auto Inc
+   id: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+     primaryKey: true,
+     autoIncrement: true
+   },
     // product_name - string and not null
+   product_name: {
+     type: DataTypes.STRING,
+     allowNull: false,
+   },
     // price - Decimal, not null, validate decimal
-    // stock - Int, not null, default value to 10
+   price: {
+     type: DataTypes.DECIMAL,
+     allowNull: false,
+     validate: {
+      isDecimal: true
+     }
+   },
+   // stock - Int, not null, default value to 10
+   stock :{
+     type: DataTypes.INTEGER,
+     allowNull: false,
+     defaultValue: '10'
+   },
     // category_id - Int and ref to Category models' ID
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+        unique: false
+      }
+    }
   },
   {
     sequelize,
