bbd5fd211aca5c91856aed575c1b74eb0ee7c0bc
kartavya <kartavyas@gmail.com>
2022-05-20 12:02:46 +1000

    updated index.js in models with associations

--- models/index.js
+++ models/index.js
@@ -4,13 +4,30 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category 
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  // When we delete a category of product, we also delete the associated product.
+  onDelete: "CASCADE",
+});
 
-// Categories have many Products
+// Categories have many Products - hasMany
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag) 
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag) 
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag-id',
+});
 
 module.exports = {
   Product,
