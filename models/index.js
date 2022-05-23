e200f3b03147a437811f862294555251278b3172
kartavya <kartavyas@gmail.com>
2022-05-23 14:35:06 +1000

    debug error

--- models/index.js
+++ models/index.js
@@ -26,7 +26,7 @@ Product.belongsToMany(Tag, {
 // Tags belongToMany Products (through ProductTag) 
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: 'tag-id',
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
